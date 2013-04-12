357f16189ffc1a77d55d8a9d8512a9724358b6b9
Bruno Pinto <brunoferreirapinto@gmail.com>
2013-04-12 14:02:26 -0300

    hiding warnings

--- download.php
+++ download.php
@@ -9,15 +9,19 @@ $data = json_decode($query, true);
 $download_link  = $data['download-link'];
 $file_name      = "/tmp/extension.alfredworkflow";
 
-$file_data = file_get_contents($download_link);
+$file_data = @file_get_contents($download_link);
 
-$handle = fopen($file_name, 'x+');
-fclose($handle);
+if ($file_data) {
+  $handle = @fopen($file_name, 'x');
+  @fclose($handle);
 
-$downloaded = file_put_contents($file_name, $file_data);
-if($downloaded > 0) {
-  exec('open '. $file_name, $extract);
+  $downloaded = @file_put_contents($file_name, $file_data);
+  if($downloaded > 0) {
+    exec('open '. $file_name, $extract);
+  } else {
+    echo "Download failed.";
+  }
 } else {
-  echo "Download failed.";
+  echo "Extension temporarily unavailable";
 }
 
